4fb9ebb844f66380479833fda9f0ce5280540908
Anton Kanin <antony.kanin@gmail.com>
2020-02-16 10:08:05 +0300

    horizontal zoom & drawing two vertical lines

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using AxisData = QVector<double>;
 
+const QPointF upVector{ 0.0, 1.0 };
+
 std::tuple<AxisData, AxisData> plotData()
 {
     int chartSize = 1000;
@@ -63,6 +65,9 @@ int main(int argc, char* argv[])
     // включаем зум по средней кнопке мыши
     cp->setInteractions(QCP::iRangeZoom);
 
+    QCPItemStraightLine* startLine{};
+    QCPItemStraightLine* endLine{};
+
     QObject::connect(cp, &QCustomPlot::mousePress, [&](QMouseEvent* event) {
         // включаем зум по прямоугольной области
         if (Qt::LeftButton == event->button())
@@ -71,7 +76,26 @@ int main(int argc, char* argv[])
             cp->setSelectionRectMode(QCP::srmNone);
         }
 
-        auto axisRect = cp->axisRectAt(event->pos());
+        if (Qt::RightButton == event->button())
+        {
+            if (!startLine)
+            {
+                qDebug() << "line";
+                const auto pos = event->pos();
+
+                startLine = new QCPItemStraightLine(cp);
+                startLine->point1->setPixelPosition(pos);
+                startLine->point2->setPixelPosition(pos + upVector);
+
+                endLine = new QCPItemStraightLine(cp);
+                endLine->point1->setPixelPosition(pos);
+                endLine->point2->setPixelPosition(pos + upVector);
+
+                cp->replot();
+            }
+        }
+
+        const auto axisRect = cp->axisRectAt(event->pos());
         if (axisRect)
         {
             // понятно, что может упасть, но у нас же есть как минимум один график =)
@@ -87,11 +111,38 @@ int main(int argc, char* argv[])
             cp->setInteractions(QCP::iRangeZoom);
             cp->setSelectionRectMode(QCP::srmZoom);
         }
+
+        if (Qt::RightButton == event->button())
+        {
+            const auto axisRect = cp->axisRectAt(event->pos());
+            if (axisRect)
+            {
+                axisRect->zoom(
+                    { startLine->point1->pixelPosition(), endLine->point1->pixelPosition() },
+                    { axisRect->axis(QCPAxis::atBottom) });
+            }
+
+            if (startLine)
+            {
+                cp->removeItem(startLine);
+                startLine = nullptr;
+
+                cp->removeItem(endLine);
+                endLine = nullptr;
+
+                cp->replot();
+            }
+        }
     });
 
     QObject::connect(cp, &QCustomPlot::mouseMove, [&](QMouseEvent* event) {
-        auto pos = event->pos();
-        qDebug() << cp->xAxis->pixelToCoord(pos.x()) << cp->yAxis->pixelToCoord(pos.y());
+        if (endLine)
+        {
+            endLine->point1->setPixelPosition(event->pos());
+            endLine->point2->setPixelPosition(event->pos() + upVector);
+
+            cp->replot();
+        }
     });
 
     window.show();
